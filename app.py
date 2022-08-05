3b74783508ae5d19608fae72b0cfe4eeca2626c5
Eoin Byrne <eoinbye@gmail.com>
2022-08-05 22:25:14 +0200

    Update

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly as plt
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import sys
